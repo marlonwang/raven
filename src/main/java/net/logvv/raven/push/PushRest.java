32c3199f5995dd44f593713b57df75aff72fe49c
marlonwang <marlonwang@163.com>
2017-01-09 23:57:00 +0800

    rest

--- src/main/java/net/logvv/raven/push/PushRest.java
+++ src/main/java/net/logvv/raven/push/PushRest.java
@@ -2,6 +2,7 @@ package net.logvv.raven.push;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,11 +13,15 @@ import net.logvv.raven.common.model.GeneralResult;
 public class PushRest {
 	private static final Logger LOGGER = LoggerFactory.getLogger(PushRest.class);
 	
+	@Autowired
+	private PushMessageService pushService;
+	
 	@RequestMapping(value = "/push/single", method=RequestMethod.POST)
 	public GeneralResult spush() 
 	{
 		GeneralResult result = new GeneralResult();
 		LOGGER.info("Begin to push single device.");
+		
 		
 		return result;
 		
