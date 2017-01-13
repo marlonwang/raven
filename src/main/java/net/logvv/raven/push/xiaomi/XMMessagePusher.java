600b37600b0bd7e3582ebeb5223c592337741096
marlonwang <marlonwang@163.com>
2017-01-13 20:30:39 +0800

    xiaomi push via regid & alias

--- src/main/java/net/logvv/raven/push/xiaomi/XMMessagePusher.java
+++ src/main/java/net/logvv/raven/push/xiaomi/XMMessagePusher.java
@@ -2,12 +2,15 @@ package net.logvv.raven.push.xiaomi;
 
 import net.logvv.raven.push.MessagePusher;
 import net.logvv.raven.push.xiaomi.xmpush.server.Message;
+import net.logvv.raven.push.xiaomi.xmpush.server.Result;
 import net.logvv.raven.push.xiaomi.xmpush.server.Sender;
 import net.logvv.raven.push.model.PushMessage;
 import net.logvv.raven.push.xiaomi.xmpush.server.Constants;
+import net.logvv.raven.utils.JsonUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * mi push
@@ -47,7 +50,7 @@ public class XMMessagePusher implements MessagePusher {
                     isSuccess = iosBroadCast(pushMessage);
                     break;
                 case UNICAST:
-                    isSuccess = iosBroadCast(pushMessage);
+                    isSuccess = iosUnicast(pushMessage);
                     break;
                 case LISTCAST:
                     isSuccess = iosListCast(pushMessage);
@@ -71,6 +74,159 @@ public class XMMessagePusher implements MessagePusher {
         return isSuccess;
     }
 
+    @Override
+    public boolean pushAndroidMessage(PushMessage pushMessage)
+    {
+        int count = 0;
+        boolean isSuccess = false;
+        while(count < RETRY_TIMES)
+        {
+            switch (pushMessage.getPushType())
+            {
+                case BROADCAST:
+                    isSuccess = androidBroadCast(pushMessage);
+                    break;
+                case UNICAST:
+                    isSuccess = androidUnicast(pushMessage);
+                    break;
+                case LISTCAST:
+                    isSuccess = androidListCast(pushMessage);
+                    break;
+                case ALIASCAST:
+                    isSuccess = androidAliasCast(pushMessage);
+                    break;
+                default:
+                    LOGGER.error("IOS Not support push type:{} ", pushMessage.getPushType());
+                    return false;
+            }
+            count ++;
+
+            if (isSuccess)
+            {
+                LOGGER.info("Send message, success:{}, try {} times.",isSuccess,count);
+                break;
+            }
+        }
+
+        return isSuccess;
+    }
+
+    // 根据设备regid推送 单个
+    private boolean androidUnicast(PushMessage pushMessage)
+    {
+        try
+        {
+            // playLoad 要发送的消息内容,透传消息必填非透传消息可选
+            Constants.useOfficial(); // 生产环境; 测试环境 Constants.useSandbox();
+            Sender sender = new Sender(androidSecretKey);
+            Message message = new Message.Builder()
+                    .title(pushMessage.getTitle())
+                    .description(pushMessage.getText())
+                    .payload(pushMessage.getText())
+                    .restrictedPackageName(androidPackageName)
+                    .passThrough(0)    // 1 透传消息 0 通知栏消息
+                    .notifyType(1)     // 使用默认提示音提示
+                    .build();
+
+            Result result = sender.send(message, pushMessage.getAudiences().get(0), 3); //根据regID，发送消息到指定设备上
+            LOGGER.info("result from xiao mi:{}", JsonUtils.obj2json(result));
+        }
+        catch (Exception e)
+        {
+            LOGGER.error("Failed to unicast, error:{}", e);
+            return false;
+        }
+        return true;
+    }
+
+    // 根据设备regid推送 单个
+    //发送消息到一组设备上, regids个数不得超过1000个
+    private boolean androidListCast(PushMessage pushMessage)
+    {
+        try{
+            Constants.useOfficial();
+            Sender sender = new Sender(androidSecretKey);
+            Message message = new Message.Builder()
+                    .title(pushMessage.getTitle())
+                    .description(pushMessage.getText())
+                    .payload(pushMessage.getText())
+                    .restrictedPackageName(androidPackageName)
+                    .notifyType(1)     // 使用默认提示音提示
+                    .passThrough(0)
+                    .build();
+            Result result = sender.send(message, pushMessage.getAudiences(), 3);
+            LOGGER.info("result from xiao mi:{}", JsonUtils.obj2json(result));
+        }catch (Exception e){
+            LOGGER.error("Failed to unicast, error:{}", e);
+            return false;
+        }
+        return true;
+    }
+
+    // 根据regid绑定的别名推送
+    private boolean androidAliasCast(PushMessage pushMessage)
+    {
+        try{
+            Constants.useOfficial();
+            Sender sender = new Sender(androidSecretKey);
+            Message message = new Message.Builder()
+                    .title(pushMessage.getTitle())
+                    .description(pushMessage.getText())
+                    .payload(pushMessage.getText())
+                    .restrictedPackageName(androidPackageName)
+                    .notifyType(1)     // 使用默认提示音提示
+                    .build();
+            Result result = sender.sendToAlias(message, pushMessage.getAudiences(), 3); //根据aliasList, 发送消息到指定设备上
+            LOGGER.info("result from xiao mi:{}", JsonUtils.obj2json(result));
+            // 一次不能超过1000
+        }catch (Exception e){
+            LOGGER.error("Failed to alias cast, error:{}", e);
+            return false;
+        }
+
+        return true;
+    }
+
+    // 标签或者全量推送
+    private boolean androidBroadCast(PushMessage pushMessage)
+    {
+        try{
+            Constants.useOfficial();
+            Sender sender = new Sender(androidSecretKey);
+            Message message = new Message.Builder()
+                    .title(pushMessage.getTitle())
+                    .description(pushMessage.getText()).payload(pushMessage.getText())
+                    .restrictedPackageName(androidPackageName)
+                    .notifyType(1)     // 使用默认提示音提示
+                    .build();
+
+            Result result = sender.broadcastAll(message, 3); //全量推送
+            //sender.broadcast(message, topic, 3); //根据topic, 发送消息到指定一组设备上
+            LOGGER.info("result from xiao mi:{}", JsonUtils.obj2json(result));
+        }catch (Exception e){
+            LOGGER.error("Failed to unicast, error:{}", e);
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean pushAllMessage(PushMessage pushMessage)
+    {
+        boolean androidResult = this.pushAndroidMessage(pushMessage);
+        if (!androidResult)
+        {
+            LOGGER.error("Android push failed.");
+        }
+        boolean iosResult = this.pushIosMessage(pushMessage);
+        if (!iosResult)
+        {
+            LOGGER.error("IOS push failed.");
+        }
+        // 只有安卓和苹果都推送失败时才返回false, 避免触发重试机制, 导致发送成功的重复推送消息
+        return !androidResult && !iosResult ? false : true;
+    }
 
     // 根据设备regid推送 单个
     private boolean iosUnicast(PushMessage pushMessage)
@@ -158,154 +314,5 @@ public class XMMessagePusher implements MessagePusher {
         }
 
         return true;
-    }
-
-    @Override
-    public boolean pushAndroidMessage(PushMessage pushMessage)
-    {
-        int count = 0;
-        boolean isSuccess = false;
-        while(count < RETRY_TIMES)
-        {
-            switch (pushMessage.getPushType())
-            {
-                case BROADCAST:
-                    isSuccess = androidBroadCast(pushMessage);
-                    break;
-                case UNICAST:
-                    isSuccess = androidUnicast(pushMessage);
-                    break;
-                case LISTCAST:
-                    isSuccess = androidListCast(pushMessage);
-                    break;
-                case ALIASCAST:
-                    isSuccess = androidAliasCast(pushMessage);
-                    break;
-                default:
-                    LOGGER.error("IOS Not support push type:{} ", pushMessage.getPushType());
-                    return false;
-            }
-            count ++;
-
-            if (isSuccess)
-            {
-                LOGGER.info("Send message, success:{}, try {} times.",isSuccess,count);
-                break;
-            }
-        }
-
-        return isSuccess;
-    }
-
-    // 根据设备regid推送 单个
-    private boolean androidUnicast(PushMessage pushMessage)
-    {
-        try
-        {
-            // playLoad 要发送的消息内容,透传消息必填非透传消息可选
-            Constants.useOfficial(); // 生产环境; 测试环境 Constants.useSandbox();
-            Sender sender = new Sender(androidSecretKey);
-            Message message = new Message.Builder()
-                    .title(pushMessage.getTitle())
-                    .description(pushMessage.getText())
-                    .payload(pushMessage.getText())
-                    .restrictedPackageName(androidPackageName)
-                    .passThrough(0)    // 1 透传消息 0 通知栏消息
-                    .notifyType(1)     // 使用默认提示音提示
-                    .build();
-
-            sender.send(message, pushMessage.getAudiences().get(0), 3).getErrorCode(); //根据regID，发送消息到指定设备上
-        }
-        catch (Exception e)
-        {
-            LOGGER.error("Failed to unicast, error:{}", e);
-            return false;
-        }
-        return true;
-    }
-
-    // 根据设备regid推送 单个
-    private boolean androidListCast(PushMessage pushMessage)
-    {
-        try{
-            Constants.useOfficial();
-            Sender sender = new Sender(androidSecretKey);
-            Message message = new Message.Builder()
-                    .title(pushMessage.getTitle())
-                    .description(pushMessage.getText())
-                    .payload(pushMessage.getText())
-                    .restrictedPackageName(androidPackageName)
-                    .notifyType(1)     // 使用默认提示音提示
-                    .passThrough(0)
-                    .build();
-            sender.send(message, pushMessage.getAudiences(), 3); //发送消息到一组设备上, regids个数不得超过1000个
-        }catch (Exception e){
-            LOGGER.error("Failed to unicast, error:{}", e);
-            return false;
-        }
-        return true;
-    }
-
-    // 根据regid绑定的别名推送
-    private boolean androidAliasCast(PushMessage pushMessage)
-    {
-        try{
-            Constants.useOfficial();
-            Sender sender = new Sender(androidSecretKey);
-            Message message = new Message.Builder()
-                    .title(pushMessage.getTitle())
-                    .description(pushMessage.getText())
-                    .payload(pushMessage.getText())
-                    .restrictedPackageName(androidPackageName)
-                    .notifyType(1)     // 使用默认提示音提示
-                    .build();
-            sender.sendToAlias(message, pushMessage.getAudiences(), 3); //根据aliasList, 发送消息到指定设备上
-            // 一次不能超过1000
-        }catch (Exception e){
-            LOGGER.error("Failed to unicast, error:{}", e);
-            return false;
-        }
-
-        return true;
-    }
-
-    // 标签或者全量推送
-    private boolean androidBroadCast(PushMessage pushMessage)
-    {
-        try{
-            Constants.useOfficial();
-            Sender sender = new Sender(androidSecretKey);
-            Message message = new Message.Builder()
-                    .title(pushMessage.getTitle())
-                    .description(pushMessage.getText()).payload(pushMessage.getText())
-                    .restrictedPackageName(androidPackageName)
-                    .notifyType(1)     // 使用默认提示音提示
-                    .build();
-
-            sender.broadcastAll(message, 3); //全量推送
-            //sender.broadcast(message, topic, 3); //根据topic, 发送消息到指定一组设备上
-        }catch (Exception e){
-            LOGGER.error("Failed to unicast, error:{}", e);
-            return false;
-        }
-
-        return true;
-    }
-
-    @Override
-    public boolean pushAllMessage(PushMessage pushMessage)
-    {
-        boolean androidResult = this.pushAndroidMessage(pushMessage);
-        if (!androidResult)
-        {
-            LOGGER.error("Android push failed.");
-        }
-        boolean iosResult = this.pushIosMessage(pushMessage);
-        if (!iosResult)
-        {
-            LOGGER.error("IOS push failed.");
-        }
-        // 只有安卓和苹果都推送失败时才返回false, 避免触发重试机制, 导致发送成功的重复推送消息
-        return !androidResult && !iosResult ? false : true;
     }
 }
--- src/main/java/net/logvv/raven/push/xiaomi/xmpush/Demo.java
+++ src/main/java/net/logvv/raven/push/xiaomi/xmpush/Demo.java
@@ -1,8 +1,10 @@
 package net.logvv.raven.push.xiaomi.xmpush;
 
+import net.logvv.raven.push.xiaomi.xmpush.server.Result;
 import net.logvv.raven.push.xiaomi.xmpush.server.Sender;
 import net.logvv.raven.push.xiaomi.xmpush.server.Constants;
 import net.logvv.raven.push.xiaomi.xmpush.server.Message;
+import net.logvv.raven.utils.JsonUtils;
 
 /**
  * Author : wangwei
@@ -11,12 +13,14 @@ import net.logvv.raven.push.xiaomi.xmpush.server.Message;
 public class Demo {
 
     private static String iosSecretKey = "" ;
-    private static String androidSecretKey = "" ;
-    private static String packageName = "";
+    private static String androidSecretKey = "bGdIYzaT7Z1gT99zv3LGTA==" ;
+    private static String packageName = "com.thirtydays.pushtest";
 
     public static void main(String[] args) {
         // regid push
         pushViaRegId("xiaomi push","this is content","23213131");
+
+        //pushViaAlias("title","push content","1122222");
     }
 
     // regid push
@@ -24,7 +28,8 @@ public class Demo {
     {
         try{
             Constants.useOfficial();
-            Sender sender = new Sender(iosSecretKey);
+            Sender sender = new Sender(androidSecretKey);
+            //Sender sender = new Sender(iosSecretKey);
             Message message = new Message.Builder()
                     .title(title)
                     .description(text)
@@ -32,7 +37,8 @@ public class Demo {
                     .restrictedPackageName(packageName)
                     .notifyType(1)     // 使用默认提示音提示
                     .build();
-            sender.sendToAlias(message, regid, 3); //根据aliasList, 发送消息到指定设备上
+            Result result = sender.sendToAlias(message, regid, 3); //根据aliasList, 发送消息到指定设备上
+            System.out.println(JsonUtils.obj2json(result));
             // 一次不能超过1000
         }catch (Exception e){
             e.printStackTrace();
@@ -40,11 +46,12 @@ public class Demo {
     }
 
     // alias push
-    private void pushViaAlias(String title,String text,String alias)
+    private static void pushViaAlias(String title,String text,String alias)
     {
         try{
             Constants.useOfficial();
-            Sender sender = new Sender(iosSecretKey);
+            //Sender sender = new Sender(iosSecretKey);
+            Sender sender = new Sender(androidSecretKey);
             Message message = new Message.Builder()
                     .title(title)
                     .description(text)
@@ -52,7 +59,8 @@ public class Demo {
                     .restrictedPackageName(packageName)
                     .notifyType(1)     // 使用默认提示音提示
                     .build();
-            sender.sendToAlias(message, alias, 3); //根据aliasList, 发送消息到指定设备上
+            Result result = sender.sendToAlias(message, alias, 3); //根据aliasList, 发送消息到指定设备上
+            System.out.println(JsonUtils.obj2json(result));
             // 一次不能超过1000
         }catch (Exception e){
             e.printStackTrace();
